b8d152136637643666d68b068d673bd7798fdd94
Alex-web-ops <66907153+Alex-web-ops@users.noreply.github.com>
2020-07-04 08:43:56 +0800

    Delete b.js

--- b.js
+++
@@ -1,9 +0,0 @@
-const {add,mul} = require('./a') //引入a.js中的加法和乘法
-const _ = require('lodash')
-const sum = add(10,20) 
-const multy = mul(100,200) 
-console.log(sum) //打印和
-console.log(multy)//打印积
-
-const arr = _.concat([1,2],3)
-console.log('arr...',arr)
